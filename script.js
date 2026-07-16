51bfb0dde6cc13e56bc852ff7b1d5b5e86fd3e84
SmitHumber <yellowpieorangesword14@gmail.com>
2026-07-16 17:25:35 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+// Mouse Events
+
+var hoverDiv = document.getElementById('hoverDiv');
+
+
+hoverDiv.addEventListener('mouseenter', function() {
+    this.innerHTML = "You See this Message";
+});
+    hoverDiv.addEventListener('mouseleave', function() {
+    this.innerHTML = "Hover Me To See This Message";
+});
+
+
+// Keyboard Events
+
+let input = document.querySelector("Input");
+let log = document.getElementById("log")
+
+input.addEventListener("keydown", logKey);
+
+
+function logKey(e) {
+    log.textContent += `${e.key}`;
+}
+
+// Form Event
+
+function formEvent(){
+
+let form = document.getElementById("form");
+let log = document.getElementById("log");
+
+function logSubmit(event) {
+    log.textContent = `Form Submitted Completed: ${event.timeStamp}`;
+    event.preventDefault();
+}
+
+form.addEventListener("submit", logSubmit);
+
+}
+
+
+// Focus and blur events
+
+function validateCurrency(){
+let currency = document.getElementById('currencyInput');
+let currencyValue = parseInt(currency.value, 10);
+
+if (currencyValue >= 50) {
+    currency.style.backgroundColor = 'green';
+    console.log('Payment Comfirmed');
+} else {
+    currency.style.backgroundColor = 'red';
+    console.log('Not Enough Payment');
+}
+
+}
+
+// Event delegation:
+
+let list = document.getElementById('numList');
+
+list.addEventListener('click', function(event) {
+    
+    if (event.target.tagName === 'LI') {
+    alert('You clicked: ' + event.target.textContent);
+                
+    event.target.style.backgroundColor = 'limegreen';
+    }
+});
+    
